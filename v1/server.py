e08dc8540841377d5dd95deb4c6de1abaece6f07
JingKun Hao <129041904+xxm1668@users.noreply.github.com>
2025-08-15 14:53:54 +0800

    Update server.py

--- v1/server.py
+++ v1/server.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 mcp = FastMCP("Math")
 
 # 替换为 DeepSeek 的 API Key 和 Base URL
-os.environ["OPENAI_API_KEY"] = "8a72a5d2-002b-4649-86eb-03957430ae3c"
+os.environ["OPENAI_API_KEY"] = "8a72a5d2-002b-4649-86eb-03957430a3c"
 os.environ["OPENAI_API_BASE"] = "https://ark.cn-beijing.volces.com/api/v3"  # 假设 DeepSeek 兼容 OpenAI
 
 client = OpenAI()
