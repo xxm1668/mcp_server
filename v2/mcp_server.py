ff0951eef1acac1bf01659d2fb4006d97902cf1b
JingKun Hao <129041904+xxm1668@users.noreply.github.com>
2025-08-15 14:51:51 +0800

    Update mcp_server.py

--- v2/mcp_server.py
+++ v2/mcp_server.py
@@ -3,7 +3,7 @@ from openai import AzureOpenAI
 
 mcp = FastMCP("Math")
 AZURE_OPENAI_ENDPOINT = 'https://llm-365ai.openai.azure.com'
-AZURE_OPENAI_API_KEY = '856174a8453543389bcfb57142b1076f'
+AZURE_OPENAI_API_KEY = '856174a8453543389bcfb57142b1076'
 client = AzureOpenAI(
     azure_endpoint=AZURE_OPENAI_ENDPOINT,
     api_key=AZURE_OPENAI_API_KEY,
